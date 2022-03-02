fe497a1596514f3d7c1d9f7eb9962a76faa603d0
anthonymruiz <anthonymruiz@hotmail.com>
2022-03-02 12:38:26 -0400

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',       component: HomeComponent, pathMatch: 'full' }, 
+  { path: 'index',  component: HomeComponent, pathMatch: 'full' },  
+  { path: 'movies', component: HomeComponent, pathMatch: 'full' },  
+  { path: "**",     component:  HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
